3993d86052b3620c92279d21995249476b6d16a5
Miguel Angel Alarcos Torrecillas <miguel.alarcos@gmail.com>
2015-03-17 03:32:07 +0100

    version 0.3.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'miguelalarcos:xdatetime',
   summary: 'datetime widget',
-  version: '0.3.3',
+  version: '0.3.4',
   git: 'https://github.com/miguelalarcos/xdatetime.git'
 });
 
